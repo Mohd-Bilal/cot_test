9416750315669648bff6efe8b27f378897610fc7
Mohd-Bilal <bilaljaleel123@gmail.com>
2018-03-05 18:06:59 +0530

    Initial commit

---
+++ src/Controller/testController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\test\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class testController extends ControllerBase {
+  /**
+   * Display test data
+   * 
+   * @return array
+   */
+
+   public function test() {
+     $select = $this -> load();
+     var_dump($select);
+     foreach($select as $entry){
+      $rows[] = array_map('Drupal\Component\Utility\SafeMarkup::checkPlain', (array) $entry);
+
+
+     }
+     return $array = array(
+       '#type' => 'table',
+       '#header' => [t('uid'),t('uuid'),t('langcode')],
+       '#row' => $rows,
+       '#empty' => t('No entries available')
+    
+     );
+     return $array = array(
+       '#type' => 'markup',
+       '#markup' => $rows
+
+     );
+    }
+   public function load(){
+    $database = \Drupal::database(); 
+   $query = $database->query("SELECT * FROM users");
+    $result = $query->fetchAll();
+    return $result;
+   } 
+}
+
+
+
+
+
